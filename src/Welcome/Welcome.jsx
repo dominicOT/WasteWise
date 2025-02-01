2029f834e9a047159fe329ba575f582ef67fd003
bohackathon <bohackathon@gmail.com>
2025-02-01 15:12:46 +0000

    four

--- src/Welcome/Welcome.jsx
+++ src/Welcome/Welcome.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import splash from "../assets/logo.png";
 import { useNavigate } from "react-router-dom";
-import "./Welcome.css";
+import "../App.css";
+
+
 
 function SplashScreen() {
   return (
-    <div className="splash-container">
-      <img src={splash} className="splash-image" alt="WasteWise" />
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-600 text-white text-2xl font-semibold">
+      <img src={splash} className="w-20" alt="WasteWise" />
+      {/* <p className="mt-2 text-lg">Loading...</p> */}
     </div>
   );
 }
@@ -14,13 +17,14 @@ function SplashScreen() {
 function MainApp() {
   const navigate = useNavigate();
   const handleButtonClick = () => {
-    navigate("/auth");
-  };
+      navigate("/auth");
+    };
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="main-app-container">
-      <div className="start-button-container">
-        <button className="start-button" onClick={handleButtonClick}>
+    <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: "url('/bg.png')" }}>
+      <div className="absolute bottom-[30%] left-1/2 transform -translate-x-1/2">
+        <button className="px-6 py-3 bg-blue-600 text-white text-lg font-semibold rounded-lg shadow-md hover:bg-blue-700 transition" onClick={handleButtonClick}>
           Get Started
         </button>
       </div>
